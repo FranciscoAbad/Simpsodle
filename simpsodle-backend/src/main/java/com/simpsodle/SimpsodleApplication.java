b59a73c913704d84613ca2dd91f936c8ba93ee58
FranciscoAbad <unlafranciscoabad@gmail.com>
2024-05-29 20:58:07 -0300

    first commit

---
+++ simpsodle-backend/src/main/java/com/simpsodle/SimpsodleApplication.java
@@ -0,0 +1,13 @@
+package com.simpsodle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpsodleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpsodleApplication.class, args);
+	}
+
+}
---
+++ simpsodle-backend/src/test/java/com/simpsodle/SimpsodleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simpsodle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpsodleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
